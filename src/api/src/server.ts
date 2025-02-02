47bc849dd8adbee48284414c6a965e973cd0d63e
zmspiler <maj@passways.io>
2025-02-02 15:18:05 +0100

    Add pino logging and replace default http server

--- src/api/src/server.ts
+++ src/api/src/server.ts
@@ -1,18 +1,21 @@
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
-import * as trpcExpress from '@trpc/server/adapters/express';
-import { appRouter } from '@/router';
 import { logger } from '@/logger';
 import { Environment } from '@/environment';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import express from 'express';
+import { appRouter } from '@/router';
+import { createContext } from '@/trpc';
+import pino from 'pino-http';
 
-const createContext = ({
-  req,
-  res,
-}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
-type Context = Awaited<ReturnType<typeof createContext>>;
+const server = express();
 
-const server = createHTTPServer({
-  router: appRouter,
-});
+server.use(pino());
+server.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
 
 server.listen(Environment.port, Environment.host, () => {
   logger.info(`Server listening on ${Environment.host}:${Environment.port}`);
